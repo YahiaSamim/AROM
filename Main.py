1dbbafd542a665fee35686a7ccd7a609dc6a2edd
DotOp3rator011 <rahulpugal.0308@gmail.com>
2018-02-01 22:39:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
 
+from Scraper import Scraper
+
+if __name__ == "__main__":
+    asin = "B073QVY9PQ"
+    url = "https://www.amazon.in/dp/" + asin
+    scraper = Scraper()
+    reviews = scraper.get_reviews(url)
+    for review in reviews:
+        print(review)
